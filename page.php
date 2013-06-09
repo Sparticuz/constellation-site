042e445474c76873bd0936bd905d842c4dab431a
Kyle McNally <kyle@kmcnally.net>
2013-06-09 16:21:25 -0400

    started working on mobile

--- page.php
+++ page.php
@@ -11,22 +11,19 @@
 							<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
 								<header class="article-header">
-
+									<?php the_post_thumbnail('large',array('class' => 'bleed')); ?>
 									<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
-									<p class="byline vcard"><?php
-										printf(__('Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>.', 'bonestheme'), get_the_time('Y-m-j'), get_the_time(__('F jS, Y', 'bonestheme')), bones_get_the_author_posts_link());
-									?></p>
-
-
 								</header> <!-- end article header -->
 
 								<section class="entry-content clearfix" itemprop="articleBody">
 									<?php the_content(); ?>
-							</section> <!-- end article section -->
+								</section> <!-- end article section -->
 
 								<footer class="article-footer">
+									<p class="byline vcard"><?php
+										printf(__('Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>.', 'bonestheme'), get_the_time('Y-m-j'), get_the_time(__('F jS, Y', 'bonestheme')), bones_get_the_author_posts_link());
+									?></p>
 									<?php the_tags('<span class="tags">' . __('Tags:', 'bonestheme') . '</span> ', ', ', ''); ?>
-
 								</footer> <!-- end article footer -->
 
 								<?php comments_template(); ?>
