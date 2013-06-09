e682f8a010e9342d3dcc808569ffb41156ae5df1
Kyle McNally <kyle@kmcnally.net>
2013-06-09 16:20:50 -0400

    added more ignores, GA, and logo

--- header.php
+++ header.php
@@ -35,6 +35,9 @@
 		<!-- end of wordpress head -->
 
 		<!-- drop Google Analytics Here -->
+		<script>(function(G,o,O,g,l){G.GoogleAnalyticsObject=O;G[O]||(G[O]=function(){(G[O].q=G[O].q||[]).push(arguments)});
+			G[O].l=+new Date;g=o.createElement('script'),l=o.scripts[0];g.src='//www.google-analytics.com/analytics.js';
+			l.parentNode.insertBefore(g,l)}(this,document,'ga'));ga('create','UA-XXXX-Y');ga('send','pageview')</script>
 		<!-- end analytics -->
 
 	</head>
@@ -47,8 +50,10 @@
 
 				<div id="inner-header" class="wrap clearfix">
 
-					<!-- to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> -->
-					<p id="logo" class="h1"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+					<!-- to use an image just replace the bloginfo('name') with your img src and remove the surrounding <p> -->
+					<a href="<?php echo home_url(); ?>" rel="nofollow" id="logo">
+						<img src="<?php echo get_template_directory_uri(); ?>/library/images/logo.png" class="aligncenter" alt="<?php bloginfo('name'); ?>"/>
+					</a>
 
 					<!-- if you'd like to use the site description you can un-comment it below -->
 					<?php // bloginfo('description'); ?>
