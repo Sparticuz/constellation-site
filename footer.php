67862dbb43470ebdeb2df7d18e7281aae4ad5462
Kyle McNally <kyle@kmcnally.net>
2013-09-21 21:32:56 -0400

    cleaned up footer & added temporary page template info

--- footer.php
+++ footer.php
@@ -1,17 +1,15 @@
-			<footer class="footer" role="contentinfo">
+			<footer class="footer clearfix" role="contentinfo">
 
 				<div id="inner-footer" class="wrap clearfix">
-
 					<?php get_sidebar(); ?>
-
 				</div> <!-- end #inner-footer -->
-				<span id="handmade-footer">
-					Handmade in the Pacific Northwest
-				</span>
+
+				<div id="handmade-footer"><span>Handmade in the Pacific Northwest</span></div>
+
 			</footer> <!-- end footer -->
 
 		</div> <!-- end #container -->
-
+		<div class="bleed">Page Template: <?php global $template; echo basename($template); ?></div>
 		<!-- all js scripts are loaded in library/bones.php -->
 		<?php wp_footer(); ?>
 
