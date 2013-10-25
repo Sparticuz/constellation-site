9c9f79ce4c30c9d3469c20ba62af839bf372acb0
Sparticuz <kyle@kmcnally.net>
2013-10-25 16:25:34 -0400

    cleaned out some bones

--- library/bones.php
+++ library/bones.php
@@ -233,8 +233,7 @@ function bones_theme_support() {
 	// registering wp3+ menus
 	register_nav_menus(
 		array(
-			'main-nav' => __( 'The Main Menu', 'bonestheme' ),   // main nav in header
-			'footer-links' => __( 'Footer Links', 'bonestheme' ) // secondary nav in footer
+			'main-nav' => __( 'The Main Menu', 'bonestheme' )   // main nav in header
 		)
 	);
 } /* end bones theme support */
@@ -262,24 +261,6 @@ function bones_main_nav() {
 	));
 } /* end bones main nav */
 
-// the footer menu (should you choose to use one)
-function bones_footer_links() {
-	// display the wp3 menu if available
-    wp_nav_menu(array(
-    	'container' => '',                              // remove nav container
-    	'container_class' => 'footer-links clearfix',   // class of container (should you choose to use it)
-    	'menu' => __( 'Footer Links', 'bonestheme' ),   // nav name
-    	'menu_class' => 'nav footer-nav clearfix',      // adding custom nav class
-    	'theme_location' => 'footer-links',             // where it's located in the theme
-    	'before' => '',                                 // before the menu
-        'after' => '',                                  // after the menu
-        'link_before' => '',                            // before each link
-        'link_after' => '',                             // after each link
-        'depth' => 0,                                   // limit the depth of the nav
-    	'fallback_cb' => 'bones_footer_links_fallback'  // fallback function
-	));
-} /* end bones footer link */
-
 // this is the fallback for header menu
 function bones_main_nav_fallback() {
 	wp_page_menu( array(
@@ -291,11 +272,6 @@ function bones_main_nav_fallback() {
         'link_before' => '',                            // before each link
         'link_after' => ''                             // after each link
 	) );
-}
-
-// this is the fallback for footer menu
-function bones_footer_links_fallback() {
-	/* you can put a default here if you like */
 }
 
 /*********************
