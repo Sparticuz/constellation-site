b24ca55c6fe41a1b1fa77c32f095d5bc9a6c832c
Brad McNally <brad@constellationco.com>
2014-03-02 16:44:57 -0800

    blocking out some content on the about page

--- page-home.php
+++ page-home.php
@@ -37,26 +37,6 @@
 		</span>
 		</section>
 
-			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-			<?php endwhile; ?>
-
-			<?php else : ?>
-
-				<article id="post-not-found" class="hentry clearfix wrap">
-					<header class="article-header">
-						<h1><?php _e("Oops, Post Not Found!", "bonestheme"); ?></h1>
-					</header>
-					<section class="entry-content">
-						<p><?php _e("Uh Oh. Something is missing. Try double checking things.", "bonestheme"); ?></p>
-					</section>
-					<footer class="article-footer">
-							<p><?php _e("This is the error message in the single-custom_type.php template.", "bonestheme"); ?></p>
-					</footer>
-				</article>
-
-			<?php endif; ?>
-
 		</div> <!-- end #main -->
 
 	</div> <!-- end #inner-content -->
