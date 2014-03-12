596c392574c3e3f1035fb441841ab7bd2ce0066a
Kyle <kyle@MacVM.local>
2014-03-12 11:43:49 -0400

    home page cleanup

--- page-home.php
+++ page-home.php
@@ -3,34 +3,33 @@
 
 <div id="content">
 
-	<figure class="hero homepage topbleed">
+	<figure class="hero homepage ceiling">
 		<img src="http://beta.constellationco.com/wp-content/uploads/2014/03/homepage_bg.jpg" class="bleed"/>
-		<figcaption class="why">Using the Historic<br>Craft of Letterpress<span class="together">to bring people together</span><img src="http://beta.constellationco.com/wp-content/uploads/2014/03/card.png" class="littleguy"></figcaption>
-		
+		<figcaption class="why">Using the Historic<br/>Craft of Letterpress<span class="together">to bring people together</span><img class="inheritWidth" src="http://beta.constellationco.com/wp-content/uploads/2014/03/card.png" class="littleguy"></figcaption>
 	</figure>
 
-<div id="inner-content" class="wrap clearfix">
+	<div id="inner-content" class="wrap clearfix">
 
 		<div id="main" class="twelve columns first clearfix" role="main">
 
-		<section class="eight column manifesto">
-		<p>Everything we do is inspired by conviction, craftsmanship, and community.</p>
-		<p>We love to make beautiful things, but our inmost desire is to facilitate meaningful connections, to use the time-honored tradition of letterpress to bring people together.</p>
-		<p>We celebrate tailor-made over generic, heritage over novelty, and process over expediency. Each of our products are printed on antique hand-fed and treadle-operated printing presses in our Seattle studio.</p>
-		<a href="/about/" class="button learnmore">Learn More</a>
-		</section>
-		<section class="three column sidebarbody">
-		<h3>Shop Our Line</h3>
-		<p>Vestibulum a nibh quis dolor rutrum congue. Maecenas fermentum eget massa in iaculis.</p>
-		<a href="">View Our Products</a>
-		<span class="">
-		<h3>Work With Us</h3>
-		<p>Vestibulum a nibh quis dolor rutrum congue. Maecenas fermentum eget massa in iaculis.</p>
-		<a href="">Request an Estimate</a>
-		<h3>Wholesale Inquiries</h3>
-		<p>Vestibulum a nibh quis dolor rutrum congue. Maecenas fermentum eget massa in iaculis.</p>
-		<a href="">Contact Us</a>
-		</section>
+			<section class="eight column manifesto">
+				<p>Everything we do is inspired by conviction, craftsmanship, and community.</p>
+				<p>We love to make beautiful things, but our inmost desire is to facilitate meaningful connections, to use the time-honored tradition of letterpress to bring people together.</p>
+				<p>We celebrate tailor-made over generic, heritage over novelty, and process over expediency. Each of our products are printed on antique hand-fed and treadle-operated printing presses in our Seattle studio.</p>
+				<a href="/about/" class="button learnmore">Learn More</a>
+			</section>
+
+			<section class="three column sidebarbody">
+				<h3>Shop Our Line</h3>
+				<p>Vestibulum a nibh quis dolor rutrum congue. Maecenas fermentum eget massa in iaculis.</p>
+				<a href="">View Our Products</a>
+				<h3>Work With Us</h3>
+				<p>Vestibulum a nibh quis dolor rutrum congue. Maecenas fermentum eget massa in iaculis.</p>
+				<a href="">Request an Estimate</a>
+				<h3>Wholesale Inquiries</h3>
+				<p>Vestibulum a nibh quis dolor rutrum congue. Maecenas fermentum eget massa in iaculis.</p>
+				<a href="">Contact Us</a>
+			</section>
 
 		</div> <!-- end #main -->
 
