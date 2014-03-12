6daa42ac8d5a6352e9fa9eda2ca1a6d0dafab418
Kyle <kyle@MacVM.local>
2014-03-12 11:40:12 -0400

    about page cleanup
    change ‘topbleed’ to ‘ceiling’

--- page-about.php
+++ page-about.php
@@ -3,110 +3,115 @@
 
 <div id="content">
 
-	<figure class="hero topbleed specialpage">
+	<figure class="hero bleed ceiling special-page">
 		<img src="http://beta.constellationco.com/wp-content/uploads/2014/03/about_bg.jpg" class="bleed"/>
-		<figcaption>About</figcaption></figure>
+		<figcaption>About</figcaption>
+	</figure>
 
-<div id="inner-content" class="wrap clearfix">
+	<div class="wrap clearfix">
+		<section class="eight column">
+			<h2>Our Studio</h2>
+			<p>Every day I walk into the studio, flip the light switch, tie my apron, and get to work. The studio is a special space—it is the fruition of a lot of dreams and hopes and hard work. It’s a place that feels like home. It’s a place infused with heritage. Our building is rich with history—it’s ornate and strong, built in the wave of new construction after the 1889 Seattle fire. I love the exposed brick, the aged wood floors, and the huge window and the natural light it lets in. But what I love most are the possibilities that this place holds. This is why I do what I love—and every day here is a blessing. We take this blessing seriously. Our work is truly a manifestation of our philosophy: to use ink and paper to connect people.</p>
+		</section>
+	</div> <!-- end #inner-content -->
+	
+	<img src="http://beta.constellationco.com/wp-content/uploads/2014/03/woodtype_bg.jpg" class="bleed" />
+	
+	<div class="wrap clearfix">
+		<section class="eight column">
+			<h2>We bring old school to another level.</h2>
+			<p class="intro">This is Gutenberg.This is the eighteenth century.This is corsets and petticoats and embroidery.</p>
+			<p>Our equipment is time-tested and well-worn, but we have restored everything with love—from rust and grime back to pristine printing condition. All of our presses are foot or hand powered and completely hand fed. Each sheet of paper goes through the press one at a time, and once through for each color printed. Same goes for type setting. We use traditional wood and lead type and carefully hand set one letter at a time. To say this is a labor of love is an understatement—but we wouldn’t have it any other way</p>
+		</section>
 
-<section class="eight column">
-<h2>Our Studio</h2>
-<p>Every day I walk into the studio, flip the light switch, tie my apron, and get to work. The studio is a special space—it is the fruition of a lot of dreams and hopes and hard work. It’s a place that feels like home. It’s a place infused with heritage. Our building is rich with history—it’s ornate and strong, built in the wave of new construction after the 1889 Seattle fire. I love the exposed brick, the aged wood floors, and the huge window and the natural light it lets in. But what I love most are the possibilities that this place holds. This is why I do what I love—and every day here is a blessing. We take this blessing seriously. Our work is truly a manifestation of our philosophy: to use ink and paper to connect people.</p>
-</section>	</div> <!-- end #inner-content -->
-<img src="http://beta.constellationco.com/wp-content/uploads/2014/03/woodtype_bg.jpg" class="bleed" />
-<div id="inner-content" class="wrap clearfix">
-<section class="eight column">
-<h2>We bring old school to another level.</h2>
-<p class="intro">This is Gutenberg.This is the eighteenth century.This is corsets and petticoats and embroidery.</p>
-<p>Our equipment is time-tested and well-worn, but we have restored everything with love—from rust and grime back to pristine printing condition. All of our presses are foot or hand powered and completely hand fed. Each sheet of paper goes through the press one at a time, and once through for each color printed. Same goes for type setting. We use traditional wood and lead type and carefully hand set one letter at a time. To say this is a labor of love is an understatement—but we wouldn’t have it any other way</p>
-</section>
+		<section class="large_img clearfix row">
+			<img src="http://placehold.it/930x655" class="twelve columns first" />
+		</section>
 
-						<section class="large_img clearfix row">
-							<img src="http://placehold.it/930x655" class="twelve columns first" />
-						</section>
-						
-						<section class="tall_img_double_wide clearfix row">
-							<img src="http://placehold.it/450x638" class="six columns first" />
-							<div class="six columns last">
-								<img src="http://placehold.it/450x304" />
-								<img src="http://placehold.it/450x304" />
-							</div>
-						</section>
+		<section class="tall_img_double_wide clearfix row">
+			<img src="http://placehold.it/450x638" class="six columns first" />
+			<div class="six columns last">
+				<img src="http://placehold.it/450x304" />
+				<img src="http://placehold.it/450x304" />
+			</div>
+		</section>
 
-<article class="eight">
-<h2>Wood Engraving</h2>
-<p>Wood engraving is a printmaking technique in which an image is worked into a block of wood. It uses relief printing, where the artist applies ink the face of the block and prints using pressure. Dating back to the 15th century, wood engraving is a perfect historic complement to letterpress printing.</p>
+		<article class="eight">
+			<h2>Wood Engraving</h2>
+			<p>Wood engraving is a printmaking technique in which an image is worked into a block of wood. It uses relief printing, where the artist applies ink the face of the block and prints using pressure. Dating back to the 15th century, wood engraving is a perfect historic complement to letterpress printing.</p>
+			<p>I really didn’t expect to fall in love with engraving. It’s infinitely satisfying to see my ideas become sketches, then printing blocks, then beautiful letterpress prints. Wood engraving and printing on the iron handpress provide me an opportunity to express myself, to experiment, and to create. My platen press is a finely tuned piece of machinery for production. It’s all business. But my iron handpress? It’s forgiving, welcoming of trial and error and artist friendly. </p>
+		</article>
+	</div> <!-- end #inner-content -->
 
-<p>I really didn’t expect to fall in love with engraving. It’s infinitely satisfying to see my ideas become sketches, then printing blocks, then beautiful letterpress prints. Wood engraving and printing on the iron handpress provide me an opportunity to express myself, to experiment, and to create. My platen press is a finely tuned piece of machinery for production. It’s all business. But my iron handpress? It’s forgiving, welcoming of trial and error and artist friendly. 
-</p>
-</article>
-</div> <!-- end #inner-content -->
-
-		<figure class="hero">
+	<figure class="hero">
 		<img src="http://beta.constellationco.com/wp-content/uploads/2014/03/products_bg.jpg" class="bleed"/>
-		<figcaption>Products</figcaption></figure>
-<div id="inner-content" class="wrap clearfix">
-<article class="eight column">
-<h2>We like to think of our cards as no matter what, no matter when cards.</h2>
-<p class="intro">These sentiments transcend occasions. You are home to me. You’re my favorite. I like you. These are anytime cards. They won’t go out of season or out of style.</p>
-<p>And they are anywhere cards, not meant to be tucked away in a drawer or tossed aside. They are pieces of art imbued with intimacy and emotion, meant for keeping and displaying. They look good framed, hung in your home. And with your sincere handwritten message? People will keep them forever.</p>
+		<figcaption>Products</figcaption>
+	</figure>
+	
+	<div class="wrap clearfix">
+		<article class="eight column">
+			<h2>We like to think of our cards as no matter what, no matter when cards.</h2>
+			<p class="intro">These sentiments transcend occasions. You are home to me. You’re my favorite. I like you. These are anytime cards. They won’t go out of season or out of style.</p>
+			<p>And they are anywhere cards, not meant to be tucked away in a drawer or tossed aside. They are pieces of art imbued with intimacy and emotion, meant for keeping and displaying. They look good framed, hung in your home. And with your sincere handwritten message? People will keep them forever.</p>
+			<p>Remember the box of cards, photos, and letters you found at your grandma’s house? I want my grandkids to find that box at my house. That heirloom ephemera won’t happen by itself, we have to build a collection. (And somehow I doubt that our grandkids will care about our online paper trail.)</p>
+			<p>A card is sweet in its sentiment and simplicity. It’s a gift, and giving is good—good for relationship. Good for the giver and for the receiver. Giving is essential to life, not for its extravagance, but for showing our loved ones we know, we care, we watch, and we listen.</p>
+		</article>
 
-<p>Remember the box of cards, photos, and letters you found at your grandma’s house? I want my grandkids to find that box at my house. That heirloom ephemera won’t happen by itself, we have to build a collection. (And somehow I doubt that our grandkids will care about our online paper trail.)</p>
+		<section class="triple_threat clearfix row">
+			<img src="http://placehold.it/290x200" class="four columns first" />
+			<img src="http://placehold.it/290x200" class="four columns" />
+			<img src="http://placehold.it/290x200" class="four columns last" />
+		</section>
 
-<p>A card is sweet in its sentiment and simplicity. It’s a gift, and giving is good—good for relationship. Good for the giver and for the receiver. Giving is essential to life, not for its extravagance, but for showing our loved ones we know, we care, we watch, and we listen.</p>
-</article>
+		<article class="eight column">
+			<h2>Product partnering</h2>
+			<p>We strongly believe that the best part of owning a business is the opportunities it creates to work with other creative people! We love to collaborate with local small businesses to create products, experiences and relationships! Our collaborations with Seattle Seed Co. and Treehouse Point have produced beautiful products that neither business could create alone. If you’re interested in collaborating, let us know! We’d love to hear from you.</p>
+		</article>
 
-<section class="triple_threat clearfix row">
-							<img src="http://placehold.it/290x200" class="four columns first" />
-							<img src="http://placehold.it/290x200" class="four columns" />
-							<img src="http://placehold.it/290x200" class="four columns last" />
-						</section>
+		<section class="double_img clearfix row">
+			<img src="http://placehold.it/450x300" class="six columns first" />
+			<img src="http://placehold.it/450x300" class="six columns last" />
+		</section>
 
-<article class="eight column">
-<h2>Product partnering</h2>
-<p>We strongly believe that the best part of owning a business is the opportunities it creates to work with other creative people! We love to collaborate with local small businesses to create products, experiences and relationships! Our collaborations with Seattle Seed Co. and Treehouse Point have produced beautiful products that neither business could create alone. If you’re interested in collaborating, let us know! We’d love
-to hear from you.</p>
-</article>
+	</div> <!-- end #inner-content -->
 
-<section class="double_img clearfix row">
-							<img src="http://placehold.it/450x300" class="six columns first" />
-							<img src="http://placehold.it/450x300" class="six columns last" />
-						</section>
-
-						</div> <!-- end #inner-content -->
-
-<figure class="hero">
+	<figure class="hero">
 		<img src="http://beta.constellationco.com/wp-content/uploads/2014/03/products_bg.jpg" class="bleed"/>
-		<figcaption>Client Work</figcaption></figure>
-<div id="inner-content" class="wrap clearfix">
-<article class="eight column">
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac mollis dolor. Etiam tellus elit, hendrerit eu odio sit amet, interdum fringilla ante. Aliquam felis tortor, condimentum eget erat vitae, faucibus sodales nisi. Cras imperdiet justo metus, nec elementum arcu facilisis non. Sed vehicula congue turpis non aliquam. Nam convallis lobortis vestibulum. Cras dignissim quam venenatis pharetra pretium. Quisque placerat imperdiet nisl, at bibendum elit pulvinar et. Vivamus cursus magna a turpis dictum, eget condimentum risus dictum. Integer nec tellus non augue auctor sagittis. Donec aliquam ipsum sit amet diam imperdiet, ac commodo leo porta.</p>
-
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac mollis dolor. Etiam tellus elit, hendrerit eu odio sit amet, interdum fringilla ante. Aliquam felis tortor, condimentum eget erat vitae, faucibus sodales nisi. Cras imperdiet justo metus, nec elementum arcu facilisis non. Sed vehicula congue turpis non aliquam. Nam convallis lobortis vestibulum. Cras dignissim quam venenatis pharetra pretium. Quisque placerat imperdiet nisl, at bibendum elit pulvinar et. Vivamus cursus magna a turpis dictum, eget condimentum risus dictum. Integer nec tellus non augue auctor sagittis. Donec aliquam ipsum sit amet diam imperdiet, ac commodo leo porta.</p>
-</article>
+		<figcaption>Client Work</figcaption>
+	</figure>
+	
+	<div class="wrap clearfix">
+		<article class="eight column">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac mollis dolor. Etiam tellus elit, hendrerit eu odio sit amet, interdum fringilla ante. Aliquam felis tortor, condimentum eget erat vitae, faucibus sodales nisi. Cras imperdiet justo metus, nec elementum arcu facilisis non. Sed vehicula congue turpis non aliquam. Nam convallis lobortis vestibulum. Cras dignissim quam venenatis pharetra pretium. Quisque placerat imperdiet nisl, at bibendum elit pulvinar et. Vivamus cursus magna a turpis dictum, eget condimentum risus dictum. Integer nec tellus non augue auctor sagittis. Donec aliquam ipsum sit amet diam imperdiet, ac commodo leo porta.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac mollis dolor. Etiam tellus elit, hendrerit eu odio sit amet, interdum fringilla ante. Aliquam felis tortor, condimentum eget erat vitae, faucibus sodales nisi. Cras imperdiet justo metus, nec elementum arcu facilisis non. Sed vehicula congue turpis non aliquam. Nam convallis lobortis vestibulum. Cras dignissim quam venenatis pharetra pretium. Quisque placerat imperdiet nisl, at bibendum elit pulvinar et. Vivamus cursus magna a turpis dictum, eget condimentum risus dictum. Integer nec tellus non augue auctor sagittis. Donec aliquam ipsum sit amet diam imperdiet, ac commodo leo porta.</p>
+		</article>
 
 		<section class="three column sidebarbody">
-		<h3>Work With Us</h3>
-		<p>Vestibulum a nibh quis dolor rutrum congue. Maecenas fermentum eget massa in iaculis.</p>
-		<a href="">Request an Estimate</a>
-		</section></div>
+			<h3>Work With Us</h3>
+			<p>Vestibulum a nibh quis dolor rutrum congue. Maecenas fermentum eget massa in iaculis.</p>
+			<a href="">Request an Estimate</a>
+		</section>
+	</div>
 
-		<span class="bigquote"><q>It’s all about life. Life shouldn’t just happen to you. You’re meant to live it, to experience the joys and the sorrows and everything in between to the fullest.</q></span>
-<div id="inner-content" class="wrap clearfix">
-<article class="twelve columns">
-<span class="four columns"><h2>Meet Our<br />Team</h2></span>
-<span class="four columns"><img src="http://placehold.it/290x200"/><h3>Sara McNally</h3>Co-Founder, Designer, & Printer</span>
-<span class="four columns"><img src="http://placehold.it/290x200"/><h3>Brad McNally</h3>Co-Founder & Secret Art Director</span></article>
-<article class="twelve columns">
-<span class="four columns"><img src="http://placehold.it/290x200"/><h3>Suzi Mantle</h3>Bookkeeping</span>
-<span class="four columns"><img src="http://placehold.it/290x200"/><h3>Holly Power</h3>Adminisitration</span>
-</article></div>
+	<span class="bigquote"><q>It’s all about life. Life shouldn’t just happen to you. You’re meant to live it, to experience the joys and the sorrows and everything in between to the fullest.</q></span>
+	
+	<div class="wrap clearfix">
+		<article class="twelve columns">
+			<span class="four columns"><h2>Meet Our<br />Team</h2></span>
+			<span class="four columns"><img src="http://placehold.it/290x200"/><h3>Sara McNally</h3>Co-Founder, Designer, &amp; Printer</span>
+			<span class="four columns"><img src="http://placehold.it/290x200"/><h3>Brad McNally</h3>Co-Founder &amp; Secret Art Director</span>
+		</article>
+		
+		<article class="twelve columns">
+			<span class="four columns"><img src="http://placehold.it/290x200"/><h3>Suzi Mantle</h3>Bookkeeping</span>
+			<span class="four columns"><img src="http://placehold.it/290x200"/><h3>Holly Power</h3>Adminisitration</span>
+		</article>
+	</div>
 
-<section class="eight">
-<h2>Seattle</h2>
-<p>This city is home to me in a way I couldn’t have predicted. Four years later, I’m still homesick when we leave and giddy when we return home. There’s nowhere in the world like Seattle. It fits us like a glove. This is where we’ve planted our feet, started our careers, grown our business, and made lifelong friends. This is where we’ll raise our kids. This is where we’ll grow old. This city—water & mountains, gold rush & midcentury modern, large yet small—it’s home.
-</p>
-</section>
+	<section class="eight">
+		<h2>Seattle</h2>
+		<p>This city is home to me in a way I couldn’t have predicted. Four years later, I’m still homesick when we leave and giddy when we return home. There’s nowhere in the world like Seattle. It fits us like a glove. This is where we’ve planted our feet, started our careers, grown our business, and made lifelong friends. This is where we’ll raise our kids. This is where we’ll grow old. This city—water &amp; mountains, gold rush &amp; midcentury modern, large yet small—it’s home.</p>
+	</section>
 
 </div> <!-- end #content -->
 
