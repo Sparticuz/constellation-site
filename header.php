12f9f5378639d2185173de53cbf85e97ce7d3528
Brad McNally <brad@constellationco.com>
2014-03-13 19:30:41 -0700

    Let's be honest, the other flywheel was too big

--- header.php
+++ header.php
@@ -1,56 +1,32 @@
 <!DOCTYPE html>
 <!--
-                                    `-:+oyhdmNNMMMMMMNNmddyso/:.                                    
-                               -/shmMMMMMMMMMMMMMMMMMMMMMMMMMMMMNdyo:`                              
-                          `:ohNMMMMMMMMMMMMMMNNNmmmmmNNMMMMMMMMMMMMMMNmy/.                          
-                       .+hNMMMMMMMMMNdhs+/-..```````````.-:+sdMMMMMMMMMMMNms:`                      
-                    .+hNMMMMMMMNhs/-``                       `.oNMMMMMMMMMMMMms:                    
-                  :yNMMMMMMNdo:``                               .dMM-.+ymMMMMMMNd+.                 
-               `/dNMMMMMMh+.`                                    .NM/   `:smMMMMMMNs-               
-             `/mMMMMMMMMMo:.`                                     mMd      `:yNMMMMMNy-             
-            :dMMMMMMMMNmNMMNmho:`                                 dMN`        .omMMMMMNs.           
-          .yNMMMMMds/...../sdNMMmy:                              `NMM.          .omMMMMMm/          
-         /mMMMMMd:`         `.+dMMNh-                            /MMm`            .sNMMMMMy.        
-       `sMMMMMN+`              `/mMMN/                          :NMM+               -hMMMMMm:       
-      .hMMMMMd-                  .mMMN.                       -sNMMs                 `oNMMMMN+      
-     .dMMMMMy`                    :MMM+                ``.-/ohNMMd/                    :mMMMMMo     
-    .dMMMMMo                      .MMMs           `-/shdmNMMMNmy/`                      -dMMMMM+    
-   `dMMMMMo                       /MMM+         .odNMMMMNmdy+-`                          .mMMMMM/   
-   yMMMMMs                       `dMMM.       `+NMMMNh+:-`                                .mMMMMN-  
-  /MMMMMh`                       +MMMh       `sMMMNs.                                      :MMMMMd` 
- `mMMMMN.                       `mMMM:       sMMMN:                                         yMMMMM+ 
- +MMMMMo                        -MMMN`      -MMMMo       ``.--::-.`                         /MMMMMm`
-`mMMMMN`                        -MMMM.      sMMMM:    -ohmNMMMMMMMmho.                      +MMMMMM/
--MMMMMs                         `mMMMh`     oMMMMy``/hMMMMMMMNNNMMMMMMh:                   .mMMMMMMh
-oMMMMM:                          -NMMMd:    -MMMMMmNMMMMMNy+:...-/smMMMMh-                /mMMMMMMMM
-hMMMMN`                           .dMMMMdo:-+MMMMyyMMMMMs`          /dMMMMy-           -omMMNhNMMMMM
-dMMMMm        `:+syhyso/.          `oNMMMMMMMNyss``yyyMMm+-`          :dMMMMms/-..-/oyNMMMNo. +MMMMM
-mMMMMm     .odMMMMMMMMMMMms-         `+dMMMMMMNs`  `sNMMMMMMdo.         -sNMMMMMMMMMMMMMh/`   /MMMMM
-dMMMMm   :hMMNho:.```-/smMMMd/          `-+yMMMy.++.hMMMMMMMMMMy.          -ohmNMMNmho:`      /MMMMM
-hMMMMM++dMNs:`           /dMMMm+`          .dMMmMMMMmMMm:/odMMMMN/                            oMMMMM
-oMMMMMMMMo`                /mMMMNy/-````-/yNMMMMMNMMMMMh    .yMMMM+                           hMMMMM
-:MMMMMMM+                   `+mMMMMNNNNNNMMMMMNy:`-NMMMN`     +MMMM-                         `NMMMMd
-`mMMMMMm                      `:sdNMMMMMMMNdy+.    dMMMN`      hMMMs                         +MMMMM+
- oMMMMMm                         ``--::--.``      `NMMMh       sMMMs                        `mMMMMN`
- `mMMMMM-                                        `yMMMN.       hMMM:                        oMMMMMo 
-  /MMMMMh                                      `:dMMMN:       -MMMd                        -NMMMMm` 
-   yMMMMMo                                 .-/sdNMMMd-        yMMN-                       .mMMMMN-  
-   `dMMMMM+                          `./ohdmNMMMMNh/`        `NMMs                       `dMMMMM+   
-    .mMMMMN+                       -odNMMMMNmdyo:.           .MMM:                      .dMMMMMo    
-     -mMMMMMs`                   -hNMMmy+:-.``               .MMM/                     -mMMMMMs     
-      .dMMMMMh.                 /NMMh/`                       hMMd.                  `+NMMMMNo      
-       `yMMMMMm/`              -NMMs`                         .mMMd-                .yMMMMMm/       
-         +NMMMMMh-             yMMh                            .yNMNy:`           .oNMMMMMh-        
-          -hMMMMMNy-           dMM:                              :ymMMds/.``  `.:smMMMMMN+`         
-            /mMMMMMNy-`        dMM.                                ./sdmNNdhhhdNMMMMMMNy.           
-             `+mMMMMMMh+.      oMM.                                    `-/oNMMMMMMMMNh:             
-               `+mMMMMMMNy/`   .MM+                                     `-oNMMMMMMNy:               
-                 `/hNMMMMMMNh+-.mMN-                                ``:sdMMMMMMMmo-                 
-                    .odNMMMMMMMMMMMNs.`                        ``-/sdNMMMMMMMNy/`                   
-                       -odNMMMMMMMMMMMds/:.````      ````.-:+sydNMMMMMMMMMmy/`                      
-                          `/sdNMMMMMMMMMMMMMNNmmdddddmmNNMMMMMMMMMMMMMNh+-                          
-                              `:+ydNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmhs/.                              
-                                    .:+oyhdmNMMMMMMMMMMNNmdys+/-`                                   
+               `-+shdmNMMMMNNdhs+:`               
+           `:smMMMNdyso++++osydMMMMmy/`           
+         :yNMMdo:`             .yMshNMMh/`        
+       /mMMMd-                   N:  :yMMNo`      
+     :mMMdo++ymdo.               Ns    `oNMN+     
+    sMMd-      -hMo             oM:      `yMMd.   
+  `dMMo          hM-        .:+dm/         :NMN-  
+ `dMM/           yM-    :ymMmho:            .NMN. 
+ yMM+           -Mm    hMd:                  -MMm 
+-MMd            sMo   sMd   `-:/:.            hMM+
+sMM:            +Mm`  dMd-omMMmmNMm+`        .NMMm
+mMM`     ``      oMNs/hMddMMo`   .sNN+`   `:sNhNMM
+NMN  -sdmddNh+`   .sdMMy``hMMds-   `+dMmmmMmo. hMM
+mMMody:`    :dMs-    -NNmmNN/smMs     `---`    dMM
+yMMM-         :hMNddNMNy:yMN   hMs            `MMm
+-MMN            `-//:.   sMd   :Md            oMM+
+ yMM+                  -sMm.   yM/           -MMm 
+ `dMM/            -+ydNNh+`   `Md           .NMN- 
+  `dMM+         :mmo:-`       `Nh          :NMN-  
+   `yMMh.      .My             /My.      `sMMd.   
+     :mMMs.    +M.              .oddo/:/yNMN+     
+       +mMMh/` -M.                 `-hMMMNs`      
+         :hMMMhoMh.              -+yNMMd+`        
+           `/ymMMMMdyo++//++oshmMMMNh+.           
+               `:+shmNMMMMMMNmdyo/.               
+
+        Conviction, Craftsmanship, & Community
 -->
 <!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
