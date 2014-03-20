fade628a3aef5981586b4eb333d58dba9eb0c8d3
Sparticuz <kyle@kmcnally.net>
2014-03-20 15:59:10 -0400

    sync both menus
    fixes #38

--- footer.php
+++ footer.php
@@ -5,7 +5,19 @@
 					<div class="two columns widget">
 						<h3>Navigate</h3>
 						<ul>
-							<?php wp_list_pages('exclude=342,331&sort_column=menu_order&title_li='); ?>
+							<?php wp_nav_menu(array(
+						    	'container' => false,                           // remove nav container
+						    	'container_class' => 'footer-menu clearfix',           // class of container (should you choose to use it)
+						    	'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
+						    	'menu_class' => 'bottom-nav clearfix',         // adding custom nav class
+						    	'theme_location' => 'main-nav',                 // where it's located in the theme
+						    	'before' => '',                                 // before the menu
+						        'after' => '',                                  // after the menu
+						        'link_before' => '',                            // before each link
+						        'link_after' => '',                             // after each link
+						        'depth' => 0,                                   // limit the depth of the nav
+						    	'fallback_cb' => 'bones_main_nav_fallback'      // fallback function
+							)); ?>
 						</ul>
 					</div>
 					<div class="three columns widget">
