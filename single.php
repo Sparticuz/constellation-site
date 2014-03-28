170f6ecc59221de203181e08f1559b23a66b8358
Sparticuz <kyle@kmcnally.net>
2014-03-28 16:18:49 -0400

    structure done for individual blog post
    fixes #16

--- single.php
+++ single.php
@@ -8,55 +8,49 @@
 
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+			<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
 
-					<header class="article-header">
-						<p class="blogdate"><?php
-							printf(__('<time class="updated" datetime="%1$s" pubdate>%2$s</time>', 'bonestheme'), get_the_time('Y-m-j'), get_the_time(get_option('date_format')));
-						?></p>
-						<h1 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
-					
+				<header class="article-header">
+					<p class="byline vcard">
+						<time class="updated" datetime="<?php echo get_the_time('Y-m-j'); ?>" pubdate><?php echo get_the_time(get_option('date_format')); ?></time>
+					</p>
+					<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+				</header> <!-- end article header -->
 
-					</header> <!-- end article header -->
+				<section class="entry-content blog-content row clearfix">
+					<?php the_content(); ?>
+					</div></div><!-- these two closing divs are important, they close .rest_of_post and .the_rest from functions.php -->
+				</section> <!-- end article section -->
 
-					<section class="entry-content clearfix" itemprop="articleBody">
-						<?php the_content(); ?>
-					</section> <!-- end article section -->
-
-					<footer class="article-footer">
-						<?php the_tags('<p class="tags"><span class="tags-title">' . __('Tags:', 'bonestheme') . '</span> ', ', ', '</p>'); ?>
-
-					</footer> <!-- end article footer -->
-
+				<footer class="nine columns comments-container" style="float:right">
 					<?php comments_template(); ?>
+				</footer>
 
-				</article> <!-- end article -->
+			</article> <!-- end article -->
 
-<footer class="article-footer">
+			<?php endwhile; ?>
 
-					</footer> <!-- end article footer -->
+			<?php bones_page_navi(); ?>
 
-				</article> <!-- end article -->
+			<?php else : ?>
 
-				<?php endwhile; ?>
+				<article id="post-not-found" class="hentry clearfix">
+					<header class="article-header">
+						<h1>Oops, Post Not Found!</h1>
+					</header>
+					<section class="entry-content">
+						<p>Uh Oh. Something is missing. Try double checking things.</p>
+					</section>
+					<footer class="article-footer">
+						<p>This is the error message in the index template.</p>
+					</footer>
+				</article>
 
-				<?php else : ?>
+			<?php endif; ?>
 
-						<article id="post-not-found" class="hentry clearfix">
-							<header class="article-header">
-								<h1><?php _e("Oops, Post Not Found!", "bonestheme"); ?></h1>
-							</header>
-							<section class="entry-content">
-								<p><?php _e("Uh Oh. Something is missing. Try double checking things.", "bonestheme"); ?></p>
-							</section>
-							<footer class="article-footer">
-									<p><?php _e("This is the error message in the single-custom_type.php template.", "bonestheme"); ?></p>
-							</footer>
-						</article>
+		</div> <!-- end #main -->
 
-				<?php endif; ?>
-
-			</div> <!-- end #main -->
+		<?php get_sidebar(); ?>
 
 	</div> <!-- end #inner-content -->
 
