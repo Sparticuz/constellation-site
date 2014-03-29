a4d6ef8f0dadda2d4ded8c0e1e640520fd50b2f7
Brad McNally <brad@constellationco.com>
2014-03-29 12:45:16 -0700

    Search box
    Fixes #56

--- functions.php
+++ functions.php
@@ -113,7 +113,7 @@ function bones_comments($comment, $args, $depth) {
 function bones_wpsearch($form) {
 	$form = '<form role="search" method="get" id="searchform" action="' . home_url( '/' ) . '" >
 	<input type="text" value="' . get_search_query() . '" name="s" id="s" placeholder="'.esc_attr__('Search Our Blog','bonestheme').'" />
-	<input type="submit" id="searchsubmit" value="'. esc_attr__('Search') .'" />
+	<input type="submit" id="searchsubmit" class="search-icon" value="'. esc_attr__('') .'" />
 	</form>';
 	return $form;
 } // don't remove this bracket!
