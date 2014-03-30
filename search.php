b71ea744a12ccec1776cca16b3b6f1422cc5f34c
Brad McNally <brad@constellationco.com>
2014-03-30 15:26:24 -0700

    fixed search results page
    fixes #57

--- search.php
+++ search.php
@@ -4,26 +4,23 @@
 
 	<div id="inner-content" class="wrap clearfix">
 
-		<div id="main" class="eight columns first clearfix" role="main">
+		<div id="main" class="nine columns first clearfix" role="main">
 			<h1 class="archive-title"><span><?php _e('Search Results for:', 'bonestheme'); ?></span> <?php echo esc_attr(get_search_query()); ?></h1>
 
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 				<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
 
-					<header class="article-header">
+				<header class="article-header">
+					<p class="byline vcard">
+						<time class="updated" datetime="<?php echo get_the_time('Y-m-j'); ?>" pubdate><?php echo get_the_time(get_option('date_format')); ?></time>
+					</p>
+					<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+				</header> <!-- end article header -->
 
-						<h3 class="search-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-						<p class="byline vcard"><?php
-							printf(__('Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme'), get_the_time('Y-m-j'), get_the_time(__('F jS, Y', 'bonestheme')), bones_get_the_author_posts_link(), get_the_category_list(', '));
-						?></p>
-
-					</header> <!-- end article header -->
-
-					<section class="entry-content">
-							<?php the_excerpt('<span class="read-more">' . __('Read more &raquo;', 'bonestheme') . '</span>'); ?>
-
-					</section> <!-- end article section -->
+				<section class="entry-content clearfix">
+					<?php the_content("Read More"); ?>
+				</section> <!-- end article section -->
 
 					<footer class="article-footer">
 
