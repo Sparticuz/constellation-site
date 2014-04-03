a436ceeb5a825d069a509e41d2e7d95419a27f0d
Brad McNally <brad@constellationco.com>
2014-04-02 21:13:50 -0700

    New thank you page

---
+++ page-thankyou.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<div id="content">
+
+	<figure class="hero homepage ceiling">
+		<img src="<?php echo get_site_url(); ?>/wp-content/uploads/2014/04/thankyou.jpg" class="bleed"/>
+		<figcaption class="thankzfig">
+  <img class="inheritWidth" src="<?php echo get_site_url(); ?>/wp-content/uploads/2014/04/mail.png"><span class="thankyou">Thank You</span><span class="sorts">For submitting an estimate request. We’ll get back to you as soon as we can.</span></figcaption>
+	</figure>
+
+</div> <!-- end #content -->
+
+<?php get_footer(); ?>
+
+
