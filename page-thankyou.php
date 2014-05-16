e63e22502ccb706185fcc7d62e64c9234429584e
Sparticuz <kyle@kmcnally.net>
2014-05-16 09:34:31 -0400

    fix formatting
    whitespace!, also missing a data-pin-no-hover

--- page-thankyou.php
+++ page-thankyou.php
@@ -5,7 +5,10 @@
 	<figure class="hero homepage ceiling">
 		<img src="<?php echo get_site_url(); ?>/wp-content/uploads/2014/04/thankyou.jpg" class="bleed" data-pin-no-hover="true"/>
 		<figcaption class="thankzfig">
-  <img class="inheritWidth" src="<?php echo get_site_url(); ?>/wp-content/uploads/2014/04/mail.png"><span class="thankyou">Thank You</span><span class="sorts">For submitting an estimate request. We’ll get back to you as soon as we can.</span></figcaption>
+			<img class="inheritWidth" src="<?php echo get_site_url(); ?>/wp-content/uploads/2014/04/mail.png" data-pin-no-hover="true"/>
+			<span class="thankyou">Thank You</span>
+			<span class="sorts">For submitting an estimate request. We’ll get back to you as soon as we can.</span>
+		</figcaption>
 	</figure>
 
 </div> <!-- end #content -->
