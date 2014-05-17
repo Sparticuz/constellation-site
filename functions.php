ffcce9d74753b22ef236d8f751f145e15e6d0a6c
Sparticuz <kyle@kmcnally.net>
2014-05-17 09:51:13 -0400

    social pretty much working on mobile
    fixes #79

--- functions.php
+++ functions.php
@@ -119,7 +119,7 @@ function bones_wpsearch($form) {
 } // don't remove this bracket!
 
 //Insert content after second paragraph of single post content.
-add_filter( 'the_content', 'prefix_insert_post_ads' );
+//This is for the meta data on a blog post
 function prefix_insert_post_ads( $content ) {
 	if ( is_single() && ! is_admin() ) {
 		$code = '<div class="the_rest row">';
@@ -151,11 +151,9 @@ function prefix_insert_post_ads( $content ) {
 		//the sharing code
 		$code.= '<span class="share_title">Share</span>';
 
-		$code.= '<style>.rrssb-buttons li.small {height: 25px;}</style>';
+		$code.= '<style>.blog-metadata .rrssb-buttons span{border-top:0;}.rrssb-buttons li a{border-radius:0;';
+		$code.= 'font-size:inherit;padding:0;}.rrssb-buttons.large-format li a .text{font-size:2em;}</style>';
 		$code.='<ul class="rrssb-buttons clearfix">';
-		
-		$code.='<li class="email"><a href="mailto:?subject='.htmlspecialchars(urlencode(html_entity_decode(get_the_title(), ENT_COMPAT, 'UTF-8')), ENT_COMPAT, 'UTF-8').'&amp;body='.urlencode(get_the_permalink()).'" class="popup">';
-		$code.='<span class="icon"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" width="28px" height="28px" viewbox="0 0 28 28" enable-background="new 0 0 28 28" xml:space="preserve"><g><path d="M20.111 26.147c-2.336 1.051-4.361 1.401-7.125 1.401c-6.462 0-12.146-4.633-12.146-12.265 c0-7.94 5.762-14.833 14.561-14.833c6.853 0 11.8 4.7 11.8 11.252c0 5.684-3.194 9.265-7.399 9.3 c-1.829 0-3.153-0.934-3.347-2.997h-0.077c-1.208 1.986-2.96 2.997-5.023 2.997c-2.532 0-4.361-1.868-4.361-5.062 c0-4.749 3.504-9.071 9.111-9.071c1.713 0 3.7 0.4 4.6 0.973l-1.169 7.203c-0.388 2.298-0.116 3.3 1 3.4 c1.673 0 3.773-2.102 3.773-6.58c0-5.061-3.27-8.994-9.303-8.994c-5.957 0-11.175 4.673-11.175 12.1 c0 6.5 4.2 10.2 10 10.201c1.986 0 4.089-0.43 5.646-1.245L20.111 26.147z M16.646 10.1 c-0.311-0.078-0.701-0.155-1.207-0.155c-2.571 0-4.595 2.53-4.595 5.529c0 1.5 0.7 2.4 1.9 2.4 c1.441 0 2.959-1.828 3.311-4.087L16.646 10.068z"></path></g></svg></span><span class="text">email</span></a></li>';
 		
 		$code.='<li class="facebook"><a href="https://www.facebook.com/sharer/sharer.php?u='.get_the_permalink().'" class="popup">';
 		$code.='<span class="icon"><?xml version="1.0" encoding="utf-8"?><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="28px" height="28px" viewbox="0 0 28 28" enable-background="new 0 0 28 28" xml:space="preserve"><path d="M27.825,4.783c0-2.427-2.182-4.608-4.608-4.608H4.783c-2.422,0-4.608,2.182-4.608,4.608v18.434	c0,2.427,2.181,4.608,4.608,4.608H14V17.379h-3.379v-4.608H14v-1.795c0-3.089,2.335-5.885,5.192-5.885h3.718v4.608h-3.726 c-0.408,0-0.884,0.492-0.884,1.236v1.836h4.609v4.608h-4.609v10.446h4.916c2.422,0,4.608-2.188,4.608-4.608V4.783z"></path></svg></span><span class="text">facebook</span></a></li>';
@@ -175,7 +173,6 @@ function prefix_insert_post_ads( $content ) {
 	}
 	return $content;
 }
-
 // Parent Function that makes the magic happen
 function prefix_insert_after_paragraph( $insertion, $paragraph_id, $content ) {
 	$closing_p = '</p>';
@@ -190,6 +187,7 @@ function prefix_insert_after_paragraph( $insertion, $paragraph_id, $content ) {
 	}
 	return implode( '', $paragraphs );
 }
+add_filter( 'the_content', 'prefix_insert_post_ads' );
 
 // the following is to add data-pin-attributes to images inside blog posts
 function add_pins_into_content($content) {
