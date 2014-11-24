38968e9c14a012491e5385c5254897c613ccff9a
Kyle McNally <kyle@kmcnally.net>
2014-11-24 14:30:51 -0500

    moved some stuff to the new structure

--- src/404.php
+++ src/404.php
@@ -3,7 +3,7 @@
 <div id="content">
 
 	<figure class="hero homepage ceiling">
-		<img src="<?php echo get_site_url(); ?>/wp-content/uploads/2014/03/sorts_bg.jpg" class="bleed"/>
+		<img src="<?php bloginfo('template_directory'); ?>/library/images/sorts_bg.jpg" class="bleed"/>
 		<figcaption class="errorfig"><span class="errorlabel">Error</span><span class="fourohfour">404</span><span class="sorts">It looks like we’re out of sorts</span></figcaption>
 	</figure>
 
