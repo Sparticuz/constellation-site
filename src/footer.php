0582e9af85bbd1ba1c756154b4dbf4a337e7649b
Kyle McNally <kyle@kmcnally.net>
2014-11-24 16:17:52 -0500

    fixed double ul on footer

--- src/footer.php
+++ src/footer.php
@@ -4,21 +4,19 @@
 					<?php //footer ?>
 					<div class="two columns widget">
 						<h3>Navigate</h3>
-						<ul>
-							<?php wp_nav_menu(array(
-						    	'container' => false,                           // remove nav container
-						    	'container_class' => 'footer-menu clearfix',           // class of container (should you choose to use it)
-						    	'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
-						    	'menu_class' => 'bottom-nav clearfix',         // adding custom nav class
-						    	'theme_location' => 'main-nav',                 // where it's located in the theme
-						    	'before' => '',                                 // before the menu
-						        'after' => '',                                  // after the menu
-						        'link_before' => '',                            // before each link
-						        'link_after' => '',                             // after each link
-						        'depth' => 0,                                   // limit the depth of the nav
-						    	'fallback_cb' => 'bones_main_nav_fallback'      // fallback function
-							)); ?>
-						</ul>
+						<?php wp_nav_menu(array(
+					    	'container' => false,                           // remove nav container
+					    	'container_class' => 'footer-menu clearfix',           // class of container (should you choose to use it)
+					    	'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
+					    	'menu_class' => 'bottom-nav clearfix',         // adding custom nav class
+					    	'theme_location' => 'main-nav',                 // where it's located in the theme
+					    	'before' => '',                                 // before the menu
+					        'after' => '',                                  // after the menu
+					        'link_before' => '',                            // before each link
+					        'link_after' => '',                             // after each link
+					        'depth' => 0,                                   // limit the depth of the nav
+					    	'fallback_cb' => 'bones_main_nav_fallback'      // fallback function
+						)); ?>
 					</div>
 					<div class="three columns widget contact-info">
 						<h3>Contact</h3>
